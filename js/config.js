5c95b0e5dd8cd1aa43ae5f102e7a1a0ac2b2e9fc
Nzinga Creatives <nzingacreatives@gmail.com>
2026-08-31 12:41:24 +0100

    Add safe runtime configuration layer for Nzinga Auth

---
+++ js/config.js
@@ -0,0 +1,6 @@
+// Public runtime configuration for the Nzinga website.
+// Keep secret/server credentials out of this file.
+window.NzingaConfig = Object.freeze({
+  supabaseUrl: '',
+  supabaseKey: ''
+});
